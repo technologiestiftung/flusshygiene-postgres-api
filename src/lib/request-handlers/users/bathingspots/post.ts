ab155a9ce73fa5efcc0d0e8879d9ca08aa8fe34b
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-03-19 13:53:14 +0100

    fix(compplexity):

--- src/lib/request-handlers/users/bathingspots/post.ts
+++ src/lib/request-handlers/users/bathingspots/post.ts
@@ -1,12 +1,9 @@
 import { Bathingspot } from './../../../../orm/entity/Bathingspot';
+import { getManager } from 'typeorm';
+import { getUserWithRelations } from '../../../repositories/custom-repo-helpers';
+import { isObject, getEntityFields, getMatchingValues } from '../../../utils';
 import { postResponse, HttpCodes, IObject } from '../../../types-interfaces';
-import { getRepository, getManager } from 'typeorm';
-import { User } from '../../../../orm/entity/User';
 import { responderWrongId, responderMissingBodyValue, responder, successResponse, errorResponse } from '../../responders';
-
-import { isObject } from '../../../utils/is-object';
-import { getEntityFields } from '../../../utils/get-entity-fields';
-import { getMatchingValues } from '../../../utils/get-matching-values-from-request';
 
 const updateFields = (spot: Bathingspot, providedValues: IObject): Bathingspot => {
   // curently silently fails needs some smarter way to set values on entities
@@ -35,44 +32,32 @@ const updateFields = (spot: Bathingspot, providedValues: IObject): Bathingspot =
   return spot;
 }
 export const addBathingspotToUser: postResponse = async (request, response) => {
+
+  const example = getEntityFields('Bathingspot');
+  if (request.body.hasOwnProperty('name') !== true || request.body.hasOwnProperty('isPublic') !== true) {
+    responderMissingBodyValue(response, example);
+  }
   try {
-    const user = await getRepository(User).findOne(request.params.userId, { relations: ['bathingspots'] });
+  const user = await getUserWithRelations(request.params.userId, ['bathingspots']);
+
     if (user === undefined) {
       responderWrongId(response);
     } else {
-      const example = getEntityFields('Bathingspot');
-
-      if (request.body.hasOwnProperty('name') !== true) {
-
-        responderMissingBodyValue(response, example);
-      }
-
-      if (request.body.hasOwnProperty('isPublic') !== true) {
-        responderMissingBodyValue(response, example);
-      }
-
       let spot = new Bathingspot();
       const filteredPropNames = await getEntityFields('Bathingspot');
-      const providedValues = getMatchingValues(request.body, filteredPropNames.props);// Object.keys(request.body)
-      try {
-        spot = updateFields(spot, providedValues);
-      } catch (err) {
-        throw err;
-      }
-
+      const providedValues = getMatchingValues(request.body, filteredPropNames.props);
+      spot = updateFields(spot, providedValues);
       const isPublic: boolean = request.body.isPublic;
       const name: string = request.body.name;
       spot.name = name;
       spot.isPublic = isPublic;
       user.bathingspots.push(spot);
-      await getManager().save(spot);
       await getManager().save(user);
-      const userAgain = await getRepository(User).findOne(request.params.userId, { relations: ['bathingspots'] });
-      if (userAgain !== undefined) {
-        responder(response, HttpCodes.successCreated, successResponse('Bathingspot created', [userAgain.bathingspots[userAgain.bathingspots.length - 1]]))
-      } else {
+      const userAgain = await getUserWithRelations(request.params.userId, ['bathingspots']);
+      if (userAgain === undefined) {
         throw Error('user id did change user does not exist anymore should never happen');
       }
+      responder(response, HttpCodes.successCreated, successResponse('Bathingspot created', [userAgain.bathingspots[userAgain.bathingspots.length - 1]]))
     }
   } catch (e) {
     responder(response, HttpCodes.internalError, errorResponse(e));
