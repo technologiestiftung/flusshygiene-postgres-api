b5b540e277229161b4dda3c94646f402504224f2
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-03-26 14:40:39 +0100

    ci:cc indent

--- src/lib/request-handlers/users/post.ts
+++ src/lib/request-handlers/users/post.ts
@@ -6,7 +6,7 @@ import { RegionRepository } from '../../repositories/RegionRepository';
 import { UserRepository } from '../../repositories/UserRepository';
 import { HttpCodes, postResponse, UserRole } from '../../types-interfaces';
 import { getEntityFields } from '../../utils/get-entity-fields';
-import { errorResponse, responder, responderMissingBodyValue, responderSuccessCreated} from '../responders';
+import { errorResponse, responder, responderMissingBodyValue, responderSuccessCreated } from '../responders';
 
 //  █████╗ ██████╗ ██████╗
 // ██╔══██╗██╔══██╗██╔══██╗
@@ -46,41 +46,41 @@ export const addUser: postResponse = async (request, response) => {
       responderMissingBodyValue(response, example);
     }
     if ((request.body.hasOwnProperty('role') === true &&
-        request.body.role === UserRole.creator)
-        &&
-       (request.body.hasOwnProperty('region') === true &&
-          list.includes(request.body.region) === true)
-      ) {
-        const region = await getRepository(Region).findOne({where: {name: request.body.region}});
-        if (region === undefined) {
-            throw new Error('region does not exist');
-          }
-        const user = createUser(request.body);
-        // user.firstName = request.body.firstName;
-        // user.lastName = request.body.lastName;
-        // user.role = request.body.role;
-        // user.email = request.body.email;
-        const errors = await validate(user);
-        if (errors.length > 0) {
-          throw new Error(`User validation failed ${JSON.stringify(errors)}`);
-        }
-        // could also be the below create event
-        // but then we can't do the validation beforehand
-        // const res = await getRepository(User).create(request.body);
-        const res = await getRepository(User).save(user); // .save(user);
-        responderSuccessCreated(response, 'User was created', [res]);
-  } else if ((request.body.hasOwnProperty('role') === true && request.body.role === UserRole.reporter)) {
-    const user = createUser(request.body);
-    const errors = await validate(user);
-    if (errors.length > 0) {
-      throw new Error(`User validation failed ${JSON.stringify(errors)}`);
-    }
-    // could also be the below create event
-    // but then we can't do the validation beforehand
-    // const res = await getRepository(User).create(request.body);
-    const res = await getRepository(User).save(user); // .save(user);
-    responderSuccessCreated(response, 'User was created', [res]);
-  } else {
+      request.body.role === UserRole.creator)
+      &&
+      (request.body.hasOwnProperty('region') === true &&
+        list.includes(request.body.region) === true)
+    ) {
+      const region = await getRepository(Region).findOne({ where: { name: request.body.region } });
+      if (region === undefined) {
+        throw new Error('region does not exist');
+      }
+      const user = createUser(request.body);
+      // user.firstName = request.body.firstName;
+      // user.lastName = request.body.lastName;
+      // user.role = request.body.role;
+      // user.email = request.body.email;
+      const errors = await validate(user);
+      if (errors.length > 0) {
+        throw new Error(`User validation failed ${JSON.stringify(errors)}`);
+      }
+      // could also be the below create event
+      // but then we can't do the validation beforehand
+      // const res = await getRepository(User).create(request.body);
+      const res = await getRepository(User).save(user); // .save(user);
+      responderSuccessCreated(response, 'User was created', [res]);
+    } else if ((request.body.hasOwnProperty('role') === true && request.body.role === UserRole.reporter)) {
+      const user = createUser(request.body);
+      const errors = await validate(user);
+      if (errors.length > 0) {
+        throw new Error(`User validation failed ${JSON.stringify(errors)}`);
+      }
+      // could also be the below create event
+      // but then we can't do the validation beforehand
+      // const res = await getRepository(User).create(request.body);
+      const res = await getRepository(User).save(user); // .save(user);
+      responderSuccessCreated(response, 'User was created', [res]);
+    } else {
       responderMissingBodyValue(response, example);
     }
 
