4e89298e383fc61f32a84d1766eca0857a5869d3
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-07-03 21:34:07 +0200

    fix: formatting

--- src/lib/utils/user-repo-helpers.ts
+++ src/lib/utils/user-repo-helpers.ts
@@ -2,14 +2,13 @@
 import { User } from '../../orm/entity/User';
 import { getRepository } from 'typeorm';
 
-export const getUserByAuth0id: (auth0id: string)=> Promise<User|undefined> = async (auth0Id) =>{
+export const getUserByAuth0id: (auth0id: string) => Promise<User | undefined> = async (auth0Id) => {
   try {
     const userRepo = getRepository(User);
-    const query = userRepo.createQueryBuilder('user').where('user.auth0Id = :auth0Id',{auth0Id});
-    // console.log(query.getSql());
+    const query = userRepo.createQueryBuilder('user').where('user.auth0Id = :auth0Id', { auth0Id });
     const user = await query.getOne();
     return user;
   } catch (error) {
-   return error;
+    return error;
   }
 }
