8fd1abc9b3ce56f56ab636a76cdf54cc8022d6e6
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-10-28 20:33:49 +0100

    fix(delete model with relations): adds deletion of MOdel

--- src/orm/entity/BathingspotModel.ts
+++ src/orm/entity/BathingspotModel.ts
@@ -7,6 +7,8 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
+  BeforeRemove,
+  getRepository,
 } from 'typeorm';
 import { Bathingspot } from './Bathingspot';
 import { RModelFile } from './RModelFile';
@@ -29,10 +31,16 @@ export class BathingspotModel {
   @Column({ type: 'text', nullable: true, select: false })
   public rmodel!: string;
 
-  @OneToMany((_type) => RModelFile, (file) => file.model, { eager: true })
+  @OneToMany((_type) => RModelFile, (file) => file.model, {
+    eager: true,
+    onDelete: 'CASCADE',
+  })
   public rmodelfiles!: RModelFile[];
 
-  @OneToMany((_type) => PlotFile, (plotFile) => plotFile.model, { eager: true })
+  @OneToMany((_type) => PlotFile, (plotFile) => plotFile.model, {
+    eager: true,
+    onDelete: 'CASCADE',
+  })
   public plotfiles!: PlotFile[];
   @Column({ type: 'text', nullable: true })
   public comment!: string;
@@ -47,4 +55,55 @@ export class BathingspotModel {
   // @OneToOne((_type) => Report, { eager: true })
   // @JoinColumn()
   // public report!: Report;
+
+  @BeforeRemove()
+  async removeAllRelations() {
+    try {
+      const plotRepo = getRepository(PlotFile);
+      const modelFilesRepo = getRepository(RModelFile);
+      const plotFiles = await plotRepo.find({ where: { modelId: this.id } });
+      const rmodelFiles = await modelFilesRepo.find({
+        where: { modelId: this.id },
+      });
+      for (const plot of plotFiles) {
+        plotRepo.remove(plot);
+      }
+      for (const rmodel of rmodelFiles) {
+        modelFilesRepo.remove(rmodel);
+      }
+      // plotFiles
+    } catch (error) {}
+    //   .then((plots) => {
+    //     plots.forEach((plot) => {
+    //       plotRepo
+    //         .remove(plot)
+    //         .then((res) => {
+    //           console.info(res);
+    //         })
+    //         .catch((err) => {
+    //           console.error(err);
+    //         });
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     console.error('Error removing plot files');
+
+    //     console.error(err);
+    //   });
+
+    // rmodelFiles
+    //   .then((files) => {
+    //     files.forEach((file) => {
+    //       modelFilesRepo
+    //         .remove(file)
+    //         .then()
+    //         .catch((err) => console.error(err));
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     console.error('Error removing model files');
+
+    //     console.error(err);
+    //   });
+  }
 }
